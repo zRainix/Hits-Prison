dc6ae39da88d4e2da61aa8d0d908d6ec72ac7db4
zRainix <luis.luerenbaum@gmail.com>
2024-01-24 16:22:25 +0100

    Reversed test commit

--- src/main/java/de/hits/customhoe/CustomHoe.java
+++ src/main/java/de/hits/customhoe/CustomHoe.java
@@ -4,16 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CustomHoe extends JavaPlugin {
 
-    public static String PREFIX = "§c[§a§lHOE§c] §7";
-
     @Override
     public void onEnable() {
-        System.out.println(PREFIX + "started");
-
     }
 
     @Override
     public void onDisable() {
-        System.out.println(PREFIX + "stopped");
     }
 }
