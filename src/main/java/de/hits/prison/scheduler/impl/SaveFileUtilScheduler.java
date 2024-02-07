c46d08fb2c605224208bc9e6455a8f61b54d24f3
zRainix <luis.luerenbaum@gmail.com>
2024-02-07 01:36:33 +0100

    Added logger to SaveFileUtilScheduler.

--- src/main/java/de/hits/prison/scheduler/impl/SaveFileUtilScheduler.java
+++ src/main/java/de/hits/prison/scheduler/impl/SaveFileUtilScheduler.java
@@ -2,20 +2,29 @@ package de.hits.prison.scheduler.impl;
 
 import de.hits.prison.scheduler.CustomScheduler;
 import de.hits.prison.util.FileUtilManager;
+import org.bukkit.Bukkit;
+
+import java.util.logging.Logger;
 
 public class SaveFileUtilScheduler extends CustomScheduler {
 
+    private Logger logger = Bukkit.getLogger();
+
     private final FileUtilManager fileUtilManager;
+
+    private static final long fiveMinutes = 5L * 60L * 20L;
+
     public SaveFileUtilScheduler(FileUtilManager fileUtilManager) {
-        super(0, 5*20L);
+        super(fiveMinutes, fiveMinutes);
 
         this.fileUtilManager = fileUtilManager;
 
-        System.out.println("yep yep");
+        logger.info("Scheduler " + getClass().getSimpleName() + " started.");
     }
 
     @Override
     public void run() {
         this.fileUtilManager.saveAll();
+        logger.info("Saved all file utils.");
     }
 }
