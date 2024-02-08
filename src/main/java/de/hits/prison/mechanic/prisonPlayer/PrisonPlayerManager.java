14e72e9ef0a0886fb0c1915e1e95ae827dcd206c
zRainix <luis.luerenbaum@gmail.com>
2024-02-08 22:04:02 +0100

    Base structure for PrisonPlayer mechanic.

---
+++ src/main/java/de/hits/prison/mechanic/prisonPlayer/PrisonPlayerManager.java
@@ -0,0 +1,24 @@
+package de.hits.prison.mechanic.prisonPlayer;
+
+import de.hits.prison.HitsPrison;
+import de.hits.prison.mechanic.helper.BaseManager;
+import org.bukkit.plugin.PluginManager;
+
+public class PrisonPlayerManager implements BaseManager {
+
+    @Override
+    public void register(HitsPrison hitsPrison, PluginManager pluginManager) {
+        // Commands:
+
+        // Listener:
+
+        // Scheduler:
+
+        /*
+        Tipp:
+
+        hitsPrison.getPrisonPlayerDao();
+        hitsPrison.getPlayerCurrencyDao();
+         */
+    }
+}
